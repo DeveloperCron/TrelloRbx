227399161d3eb4502326ca513d29de71476c1498
Dev_Cron <79309515+DeveloperCron@users.noreply.github.com>
2022-08-14 17:00:40 +0300

    Last Test

--- src/Hello.lua
+++ src/Hello.lua
@@ -1,8 +1,9 @@
 local function Get(Dude)
-    if not Dude then return end
-    print(
-        "Hello"
-    )
+    if not Dude then
+        return 
+    end
+
+    print("Hello World")
 end
 
 return Get
